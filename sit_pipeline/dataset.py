82dbf6a32ae25aa86b851fdaea8d1f82a84a38c4
amndzdzdz <zorrofx@gmx.net>
2025-07-01 13:00:44 +0200

    updated dataset class with new tiling + standardization of poses

--- sit_pipeline/dataset.py
+++ sit_pipeline/dataset.py
@@ -5,8 +5,7 @@ from torchvision import transforms
 import torch
 
 class SiTDataset(Dataset):
-
-    def __init__(self, pth=None, transform=None, image_size=256):   
+    def __init__(self, pth=None, transform=None, image_size=128):   
         self.pth = []
         self.images = []
         self.poses = []
@@ -15,6 +14,9 @@ class SiTDataset(Dataset):
 
         self.image_size = image_size                      
         self.latent_size = image_size // 8                
+
+        self.pose_mean = None
+        self.pose_std = None
 
         if pth is not None:
             self.pth = pth
@@ -30,33 +32,50 @@ class SiTDataset(Dataset):
 
             self.focal_length = loaded["focal"]
             self.images = np.concatenate(images_list, axis=0)
-            self.poses = np.concatenate(poses_list, axis=0)
+            self.poses = np.concatenate(poses_list, axis=0)  # shape: (N, 4, 4)
+
+            self.compute_pose_stats()
+
+    def compute_pose_stats(self):
+        poses_flat = self.poses.reshape(len(self.poses), -1)  # shape: (N, 16)
+        self.pose_mean = poses_flat.mean(axis=0)              # shape: (16,)
+        self.pose_std = poses_flat.std(axis=0) + 1e-8         # avoid division by zero
 
     def __getitem__(self, index):
         image = self.images[index]
-        pose = self.poses[index]
+        pose = self.poses[index]  # shape: (4, 4)
+        gt_pose = pose.copy()
 
         if self.transform:
             image = self._to_pil_image(image)
             image = self.transform(image)
 
-        noise = self._make_gaussian_noise(0, 1, (4, self.latent_size, self.latent_size))   
+        pose_flat = pose.reshape(-1) 
+        pose_normalized = (pose_flat - self.pose_mean) / self.pose_std
 
-        pose = pose.reshape(1,16)
-        pose_block = np.tile(pose, (self.latent_size * self.latent_size // 16, 1)).reshape(self.latent_size, self.latent_size)  # (latent_size, latent_size)
-        pose = np.stack([pose_block]*4)
+        latent_size = self.latent_size
+        pose_tensor = np.zeros((4, latent_size, latent_size), dtype=np.float32)
 
-        pose = pose + noise
+        quadrant_size = latent_size // 2
+        idx = 0
+        for c in range(4):
+            for i in range(2):
+                for j in range(2):
+                    val = pose_normalized[c * 4 + i * 2 + j]
+                    pose_tensor[c, i*quadrant_size:(i+1)*quadrant_size,
+                                   j*quadrant_size:(j+1)*quadrant_size] = val
+        
+        noise = self._make_gaussian_noise(0, 1, pose_tensor.shape)
+        gaussian_pose = pose_tensor + noise
+        gaussian_pose = torch.tensor(gaussian_pose, dtype=torch.float32)
 
-        pose = torch.tensor(pose, dtype=torch.float32)
-
-        return image, pose
+        return image, gaussian_pose, gt_pose, noise
 
     def _to_pil_image(self, image):
         return transforms.ToPILImage()(image)
 
     def _make_gaussian_noise(self, mean: float, std: float, size: tuple):
-        noise = np.random.normal(mean, std, size)
+        noise = np.random.normal(mean, std, size).astype(np.float32)
         return noise
 
     def __len__(self):
@@ -65,4 +84,4 @@ class SiTDataset(Dataset):
         return 0
 
     def save(self):
-        np.savez_compressed(self.pth, images = self.images, poses = self.poses, focal = self.focal_length)
+        np.savez_compressed(self.pth, images=self.images, poses=self.poses, focal=self.focal_length)
